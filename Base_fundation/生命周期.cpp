22a8d01856c68f52ffb8f1dccb69d872800df0ce
HuangJuncheng <124542214+Darren617@users.noreply.github.com>
2026-07-24 10:49:41 +0800

    Update 生命周期.cpp

--- Base_fundation/生命周期.cpp
+++ Base_fundation/生命周期.cpp
@@ -11,8 +11,24 @@ public:
         cout<<"destroyed entity!"<<endl;
     }
 };
+//作用域指针
+class Scopedptr{
+private:
+    Entity *m_ptr;
+public:
+    Scopedptr(Entity * ptr):m_ptr(ptr){
+
+    }   
+    
+    ~Scopedptr(){
+        delete m_ptr;
+    }
+};
 
 int main(){
-    Entity e;
+    //Entity e;
+    Scopedptr e(new Entity());//栈对象，受作用域管控
+    //Entity* e=new Entity();//普通裸指针
     cin.get();
+    //delete e;
 }
